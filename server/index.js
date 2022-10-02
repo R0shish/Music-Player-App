d45c0409f4069ae3238cce50beb0d40eea0de578
Roshish Shrestha <roshish152002@gmail.com>
2022-10-03 00:46:43 +0545

    add: songs url updated

--- server/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3500;
-const cors = require("cors");
-
-app.get("/", (req, res) => res.json("Server is running..."))
-
-app.listen(PORT, () => console.log(`Listening to port ${PORT}`))
