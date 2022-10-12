5f4eb3841743835685bd8ed8ef47432eaae8a5ee
Roshish Shrestha <roshish152002@gmail.com>
2022-10-12 22:49:05 +0545

    refactor: change API constant from localhost to heroku

--- client/lib/constants/api_constant.dart
+++ client/lib/constants/api_constant.dart
@@ -1,8 +1,12 @@
-import 'dart:io';
+// import 'dart:io';
 
 class Api {
-  static String baseURL =
-      Platform.isAndroid ? "http://10.0.2.2:3500" : "http://localhost:3500";
+  // API on localhost
+  // static String baseURL =
+  //     Platform.isAndroid ? "http://10.0.2.2:3500" : "http://localhost:3500";
+
+  // API Hosted on Heroku
+  static String baseURL = "https://flutter-music-player-app.herokuapp.com";
   static String login = "$baseURL/api/auth/login";
   static String register = "$baseURL/api/auth/register";
   static String playlist = "$baseURL/api/playlist";
